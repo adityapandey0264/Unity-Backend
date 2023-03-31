affffc59d1dc5c884d538896083fdf779785bc47
DESKTOP-399M2SR\Admin <adityapandey0264@gmail.com>
2023-03-31 07:49:02 +0530

    GetDate Php

---
+++ Unity_Backend/Assets/DateWeb.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class DateWeb : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GetDate());
+    }
+
+    IEnumerator GetDate()
+    {
+        using (UnityWebRequest www=UnityWebRequest.Get("http://localhost/Unity%20Backend/Date.php"))
+        {
+            yield return www.Send();
+            if(www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                //Show result
+                Debug.Log(www.downloadHandler.text);
+                // Or Retrieve result as binary Data.
+                byte[] results = www.downloadHandler.data;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
